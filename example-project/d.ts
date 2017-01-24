39e62309fb433cb53f870e87a7ad3bc83135d03e
Rasmus Andersson <rasmus@figma.com>
2017-01-24 00:50:17 -0800

    Fixes a few outstanding things for external module loading, including unification of multiple imports into one

--- example-project/d.ts
+++ example-project/d.ts
@@ -1,8 +1,10 @@
 import {f} from './f'
 import {inspect} from 'util'
 import x from 'foo'
+import {createContext} from 'vm'
 
 export function d(v :any) {
   console.log('d:', inspect(v), 'foo:', x)
+  createContext()
   return f(v * 2)
 }
--- example-project/e.ts
+++ example-project/e.ts
@@ -1,6 +1,16 @@
+// const cr = require('crypto')
+import {createContext} from 'vm'
+
 function e(v :any) {
   // not used and not exported, so should be completely eliminated
+  createContext()
+  // return v * 4 * cr
   return v * 4
 }
 
-console.log('hello from e')
+console.log('hello from e' && require('crypto') || require('./c').c)
+
+const cr = require('crypto')
+function x() {
+  return cr
+}
